4b51533810b77e508f27cab288bbc012fa1dcc4d
10cheon00 <10cheon00@gmail.com>
2023-04-13 13:44:44 +0900

    format: css-manager.js

--- src/engine/core/css-manager.js
+++ src/engine/core/css-manager.js
@@ -1,30 +1,32 @@
+import Vector from "/src/engine/core/vector.js";
+
 /*
  * html element의 style 즉, css를 통제하는 객체다.
  * 이 객체가 생성될 때 하나의 element에 연결되어 그 element의 style를 통제한다.
  */
 export default class CSSManager {
-  constructor(element){
+  constructor(element) {
     this.element = element;
 
     this.element.style.display = "inline-block";
     this.element.style.position = "absolute";
   }
 
-  changeTransformOrigin(origin){
+  changeTransformOrigin(origin) {
     this.element.style.transformOrigin = origin;
   }
 
   /*
-   * position, scale, rotation을 
+   * position, scale, rotation을
    * matrix에 적용하기 위해 연산 과정을 거친 후
    * transform: matrix(...) 처럼 사용가능하도록
    * 문자열로 변환하여 적용한다.
-   * 
+   *
    * 아래 url에 matrix함수가 어떻게 동작하는지 나와있다.
    * https://angrytools.com/css-generator/transform/
    */
-  updateMatrix(matrix){
-    const radian = matrix.rotation*(Math.PI / 180); // degree to radian
+  updateMatrix(matrix) {
+    const radian = matrix.rotation * (Math.PI / 180); // degree to radian
     const values = [
       matrix.scale.x * Math.cos(radian),
       matrix.scale.x * -Math.sin(radian),
