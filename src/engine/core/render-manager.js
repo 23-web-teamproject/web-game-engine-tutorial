91d23aff450c11ddcb357169567772f34717bc50
10cheon00 <10cheon00@gmail.com>
2023-04-18 15:03:50 +0900

    feat: add buffer canvas
    Buffer canvas (a.k.a offscree canvas) will provide area to blend image
    with color overlay.

--- src/engine/core/render-manager.js
+++ src/engine/core/render-manager.js
@@ -2,11 +2,27 @@ import SceneManager from "/src/engine/core/scene-manager.js";
 
 export default class RenderManager {
   static renderTargetId = "render-target";
+  static renderTarget = undefined;
+  static renderTargetWidth = 1280;
+  static renderTargetHeight = 720;
+  static bufferRenderTarget = undefined;
+  static bufferRenderTargetId = "buffer-render-target";
 
   constructor() {}
 
   static getCurrentRenderTarget() {
-    return document.getElementById(RenderManager.renderTargetId);
+    if (RenderManager.renderTarget === undefined) {
+      RenderManager.renderTarget = document.getElementById(
+        RenderManager.renderTargetId
+      );
+    }
+
+    if (RenderManager.renderTarget === undefined) {
+      RenderManager.renderTarget = document.createElement("canvas");
+      RenderManager.renderTarget.id = RenderManager.renderTargetId;
+    }
+
+    return RenderManager.renderTarget;
   }
 
   static clearScreen() {
@@ -16,5 +32,41 @@ export default class RenderManager {
 
   static render() {
     SceneManager.getCurrentScene().render();
+    const canvas = RenderManager.getCurrentRenderTarget();
+  }
+
+  static getBufferRenderTarget() {
+    if (RenderManager.bufferRenderTarget === undefined) {
+      RenderManager.bufferRenderTarget = document.getElementById(
+        RenderManager.bufferRenderTargetId
+      );
+    }
+
+    if (
+      RenderManager.bufferRenderTarget === undefined ||
+      RenderManager.bufferRenderTarget === null
+    ) {
+      const bufferRenderTarget = document.createElement("canvas");
+      bufferRenderTarget.id = RenderManager.bufferRenderTargetId;
+      RenderManager.bufferRenderTarget = bufferRenderTarget;
+    }
+    return RenderManager.bufferRenderTarget;
+  }
+
+  static changeResolution(width, height) {
+    RenderManager.renderTargetWidth = width;
+    RenderManager.renderTargetHeight = height;
+
+    const renderTarget = RenderManager.getCurrentRenderTarget();
+    renderTarget.width = RenderManager.renderTargetWidth;
+    renderTarget.height = RenderManager.renderTargetHeight;
+
+    RenderManager.changeBufferRenderTargetResolution(width, height);
+  }
+
+  static changeBufferRenderTargetResolution(width, height) {
+    const bufferRenderTarget = RenderManager.getBufferRenderTarget();
+    bufferRenderTarget.width = width;
+    bufferRenderTarget.height = height;
   }
 }
