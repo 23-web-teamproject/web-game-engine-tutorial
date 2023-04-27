f3911ba8f8d006da8344c645c93dfbecac1bf9af
10cheon00 <10cheon00@gmail.com>
2023-04-27 23:16:45 +0900

    comment: revise comment for new init method

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@
  * main.js는 index.html에서 명시적으로 불러오고 있다.
  * 따라서 절대 지워서는 안된다.
  * 
- * main.js는 게임을 실행하기 전에 어떤 씬을 불러올 것인지를 결정한다.
- * 어떤 씬을 불러오려면 Engine내의 코드를 수정하지 않고,
- * registerInitialScene에 다른 씬 객체를 넣으면 된다.
+ * main.js는 게임을 실행하기 위해 기본적인 환경을 설정하는 역할을 한다.
+ * init함수에 설정값을 입력하여 엔진을 초기화한다.
+ * 이 때 최초로 불러올 씬을 선택하여 게임을 시작한다.
  */
 import Engine from "/src/engine/engine.js";
 import ExampleScene from "/src/example-scene/scene.js";
