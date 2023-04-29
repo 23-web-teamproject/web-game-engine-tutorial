0363e71581e01ffa57a6643c4259e8c679736117
10cheon00 <10cheon00@gmail.com>
2023-04-29 14:04:56 +0900

    fix: update `ParticleEffect` to use options parameter
    To check and clamp parameters, `typeCheck` and `typeCheckAndClamp`
    method are added in helper.js

--- src/engine/fx/particle-effect.js
+++ src/engine/fx/particle-effect.js
@@ -7,51 +7,57 @@
 import GameObject from "/src/engine/core/game-object.js";
 import Sprite from "/src/engine/core/sprite.js";
 import Vector from "/src/engine/data-structure/vector.js";
+import { typeCheck, typeCheckAndClamp } from "/src/engine/utils.js";
 
 class Particle extends Sprite {
   constructor(options) {
-    super(options.imageSrc);
-    this.options = options;
+    super(options);
     this.lifeTime = options.lifeTime;
+    this.initialLifeTime = options.lifeTime;
+    this.isScaleFade = options.isScaleFade;
+    this.isAlphaFade = options.isAlphaFade;
+
     this.direction =
       options.direction -
       options.diffuseness +
       Math.random() * options.diffuseness * 2;
+
     this.speed = options.speed;
+
+    const rad = -(this.direction * Math.PI) / 180;
+
+    const forward = new Vector(
+      Math.cos(rad) * this.speed,
+      Math.sin(rad) * this.speed
+    );
+
+    this.addVelocity(forward);
   }
 
   update(deltaTime) {
-    this.lifeTime -= deltaTime;
-    this.spreadOut(deltaTime);
     this.fadeAway();
 
+    this.lifeTime -= deltaTime;
     if (this.lifeTime < 0) {
       this.destroy();
     }
   }
 
   draw() {
-
     super.draw();
   }
 
-  spreadOut(deltaTime) {
-    const rad = (this.direction * Math.PI) / 180;
-    const forward = new Vector(
-      Math.cos(rad) * this.speed * deltaTime,
-      Math.sin(rad) * this.speed * deltaTime
-    );
-    this.addPos(forward.x, forward.y);
-  }
-
   fadeAway() {
-    if (this.options.isScaleFade) {
+    if (this.isScaleFade) {
       this.setScale(
-        this.lifeTime / this.options.lifeTime,
-        this.lifeTime / this.options.lifeTime
+        new Vector(
+          this.lifeTime / this.initialLifeTime,
+          this.lifeTime / this.initialLifeTime
+        )
       );
     }
-    if (this.options.isAlphaFade) {
+    if (this.isAlphaFade) {
+      this.color.a = this.lifeTime / this.initialLifeTime;
       // TODO
       // alpha값 조절할 방법 추가해야함.
     }
@@ -60,74 +66,88 @@ class Particle extends Sprite {
 
 export default class ParticleEffect extends GameObject {
   constructor(options) {
-    super();
+    super(options);
 
     this.defaultParticleImage = "/src/engine/assets/defaultParticleImage.png";
 
-    this.options = {
-      /*
-       * 파티클이 생성되려면 isEnable이 true여야 한다. 기본값은 false다.
-       */
-      isEnable: false,
-
-      /*
-       * 파티클이 점점 크기가 작아질건지를 의미한다.
-       * 기본값은 true다.
-       */
-      isScaleFade: true,
-
-      /*
-       * 파티클이 점점 투명해질건지를 의미한다.
-       * 기본값은 true다.
-       */
-      isAlphaFade: true,
-
-      /*
-       * 1초동안 생성할 파티클의 개수를 의미한다.
-       * 1 ~ 100 사이의 값을 지정할 수 있다. 기본값은 10이다.
-       * 파티클 생성 주기는 이 값에 의해 결정된다.
-       */
-      numberOverOneSecond: 30,
-
-      /*
-       * 파티클이 생성되어 퍼져나가는 주 방향을 의미한다.
-       * 0 ~ 360 사이의 값을 지정할 수 있다. 기본값은 0(degree)이다.
-       */
-      direction: 0,
-
-      /*
-       * 파티클이 생성되고 나서 퍼져나갈 때 퍼짐 정도를 의미한다.
-       * 파티클이 퍼져나가는 방향은 최종적으로 아래에 명시된 범위이다.
-       * direction - diffuseness ~ direction + diffuseness
-       *
-       * 만약 direction이 90도이고 diffuseness가 30도라면
-       * 파티클은 60도 ~ 120도 사이 방향중에서 랜덤하게 결정된 방향으로 퍼진다.
-       * 0 ~ 180 사이의 값을 지정할 수 있다. 기본값은 30(degree)이다
-       */
-      diffuseness: 15,
-
-      /*
-       * 파티클이 날아가는 속도를 의미한다.
-       * 값이 낮을수록 파티클이 멀리 퍼지지 않게 된다.
-       * 100 ~ 1000 사이의 값을 지정할 수 있다. 기본값은 100이다.
-       */
-      speed: 100,
-
-      /*
-       * lifeTime은 생성된 파티클이 몇 초동안 화면에 보일지를 의미한다.
-       * 0.1 ~ 5.0 사이의 값을 지정할 수 있다. 기본값은 3초다.
-       */
-      lifeTime: 3,
-
-      /*
-       * 파티클의 이미지 src를 의미한다.
-       * 기본값으로는 엔진에 딸린 이미지를 불러오게 된다.
-       */
-      imageSrc: this.defaultParticleImage,
-    };
+    /*
+     * 파티클이 생성되려면 isEnable이 true여야 한다. 기본값은 false다.
+     */
+    this.isEnable = typeCheck(options.isEnable, "boolean", false);
+    /*
+     * 파티클이 점점 크기가 작아질건지를 의미한다.
+     * 기본값은 true다.
+     */
+    this.isScaleFade = typeCheck(options.isScaleFade, "boolean", true);
+    /*
+     * 파티클이 점점 투명해질건지를 의미한다.
+     * 기본값은 true다.
+     */
+    this.isAlphaFade = typeCheck(options.isAlphaFade, "boolean", true);
+    /*
+     * 1초동안 생성할 파티클의 개수를 의미한다.
+     * 1 ~ 100 사이의 값을 지정할 수 있다. 기본값은 10이다.
+     * 파티클 생성 주기는 이 값에 의해 결정된다.
+     */
+    this.countPerSecond = typeCheckAndClamp(
+      options.countPerSecond,
+      "number",
+      30,
+      1,
+      100
+    );
+    /*
+     * 파티클이 생성되어 퍼져나가는 주 방향을 의미한다.
+     * 0 ~ 360 사이의 값을 지정할 수 있다. 기본값은 0(degree)이다.
+     */
+    this.direction = typeCheckAndClamp(options.direction, "number", 0, 0, 360);
+    /*
+     * 파티클이 생성되고 나서 퍼져나갈 때 퍼짐 정도를 의미한다.
+     * 파티클이 퍼져나가는 방향은 최종적으로 아래에 명시된 범위이다.
+     * direction - diffuseness ~ direction + diffuseness
+     *
+     * 만약 direction이 90도이고 diffuseness가 30도라면
+     * 파티클은 60도 ~ 120도 사이 방향중에서 랜덤하게 결정된 방향으로 퍼진다.
+     * 0 ~ 180 사이의 값을 지정할 수 있다. 기본값은 30(degree)이다
+     */
+    this.diffuseness = typeCheckAndClamp(
+      options.diffuseness,
+      "number",
+      30,
+      0,
+      180
+    );
+    /*
+     * 파티클이 날아가는 속도를 의미한다.
+     * 값이 낮을수록 파티클이 멀리 퍼지지 않게 된다.
+     * 100 ~ 1000 사이의 값을 지정할 수 있다. 기본값은 100이다.
+     */
+    this.speed = typeCheckAndClamp(options.speed, "number", 100, 100, 1000);
+    /*
+     * lifeTime은 생성된 파티클이 몇 초동안 화면에 보일지를 의미한다.
+     * 0.1 ~ 5.0 사이의 값을 지정할 수 있다. 기본값은 3초다.
+     */
+    this.lifeTime = typeCheckAndClamp(options.lifeTime, "number", 3, 0.1, 5);
+    /*
+     * 파티클의 이미지 경로를 의미한다.
+     * 기본값으로는 엔진에 딸린 이미지를 불러오게 된다.
+     */
+    this.imagePath = typeCheck(
+      options.imagePath,
+      "string",
+      this.defaultParticleImage
+    );
+    /*
+     * 만약 enablePhysics가 true라면, 파티클에도 물리효과를 적용해야한다.
+     * 기본값은 false다.
+     */
+    this.isParticlePhysicsEnable = typeCheck(
+      options.isParticlePhysicsEnable,
+      "boolean",
+      false
+    );
 
     this.elapsedTime = 0;
-    this.currentParticleCount = 0;
   }
 
   update(deltaTime) {
@@ -139,10 +159,20 @@ export default class ParticleEffect extends GameObject {
       // 누적된 시간이 unitTime보다 커질 때에만 파티클을 생성한다.
       if (this.elapsedTime > this.unitTime) {
         this.elapsedTime %= this.unitTime;
-        const newParticle = new Particle(this.options);
+        const options = {
+          direction: this.direction,
+          diffuseness: this.diffuseness,
+          speed: this.speed,
+          lifeTime: this.lifeTime,
+          enablePhysics: this.isParticlePhysicsEnable,
+          isAlphaFade: this.isAlphaFade,
+          isScaleFade: this.isScaleFade,
+          imagePath: this.imagePath,
+        };
+        const newParticle = new Particle(options);
         // TODO
         // transformOrigin과 같은 속성이 좌상단으로 되어있다면?
-        // 
+        //
         // const centerPos = newParticle.getCenterPos();
         // newParticle.addPos(-centerPos.x, -centerPos.y);
         this.addChild(newParticle);
@@ -156,9 +186,9 @@ export default class ParticleEffect extends GameObject {
 
   run() {
     this.isEnable = true;
-    // numberOverOneSecond를 이용해서 파티클 1개를 생성하려면
+    // countPerSecond를 이용해서 파티클 1개를 생성하려면
     // 정확히 몇 초가 지나야지만 생성할 수 있는지 미리 계산한다.
-    this.unitTime = 1 / this.options.numberOverOneSecond;
+    this.unitTime = 1 / this.countPerSecond;
     this.elapsedTime = 0;
   }
 
--- src/engine/utils/helper.js
+++ src/engine/utils/helper.js
@@ -2,6 +2,29 @@ const clamp = (value, min, max) => {
   return Math.min(max, Math.max(min, value));
 }
 
+const typeCheck = (value, Type, defaultValue) => {
+  if(typeof Type === "object") {
+    if(value instanceof Type) {
+      return value;
+    } else {
+      return defaultValue;
+    }
+  } else {
+    if(typeof value === Type) {
+      return value;
+    } else {
+      return defaultValue;
+    }
+  }
+}
+
+const typeCheckAndClamp = (value, Type, defaultValue, min, max) => {
+  const typeCheckedValue = typeCheck(value, Type, defaultValue);
+  return clamp(typeCheckedValue, min, max);
+}
+
 export {
-  clamp
+  clamp,
+  typeCheck,
+  typeCheckAndClamp
 };
