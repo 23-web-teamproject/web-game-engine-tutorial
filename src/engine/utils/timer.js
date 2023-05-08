1b0966e5efe79ab3583bcbb5a4796bc82644812b
10cheon00 <10cheon00@gmail.com>
2023-05-08 12:50:27 +0900

    comment: modify comment to follow JSDoc

--- src/engine/utils/timer.js
+++ src/engine/utils/timer.js
@@ -1,7 +1,4 @@
-/*
- * 이 객체는 이전 프레임과 현재 프레임의 시간차인
- *   deltaTime을 계산하는 일을 담당한다.
- *
+/**
  * 게임에서 1 프레임마다 캐릭터가 10픽셀씩 이동한다고 가정하자.
  * 60fps일 때는 1초에 60번 게임이 업데이트되므로,
  * 10픽셀 * 60 = 600픽셀만큼 캐릭터가 이동하게 된다.
@@ -45,41 +42,59 @@
  */
 import { typeCheckAndClamp } from "/src/engine/utils.js";
 
+/**
+ * 이 객체는 이전 프레임과 현재 프레임의 시간차인
+ * deltaTime을 계산하는 일을 담당한다.
+ */
 export default class Timer {
   constructor() {
-    /*
+    /**
+     * @property {number} currentTime
+     *
      * 현재 프레임의 시간을 말한다.
      * 웹페이지가 열린 후 지난 시간이 저장된다.
      */
     this.currentTime = this.getCurrentTime();
-    /*
+    /**
+     * @property {number} currentTime
+     *
      * 이전 프레임의 시간을 말한다.
      */
     this.previousTime = this.currentTime;
-    /*
+    /**
+     * @property {number} deltaTime
+     *
      * 현재 프레임의 시간과 이전 프레임의 시간의 차를 말한다.
      */
     this.deltaTime = 0;
-    /*
+    /**
+     * @property {number} accumulatedTime
+     *
      * 매 프레임마다 누적된 deltaTime을 저장한다.
      */
     this.accumulatedTime = 0;
-    /*
+    /**
+     * @property {number} fps
+     *
      * 1초에 보여줄 프레임의 개수를 말한다.
      * 기본값으로 60이고, 24부터 MAX_VALUE 사이의 값을 저장할 수 있다.
      */
+    this.fps = 24;
     this.setFps(60);
-    /*
+    /**
+     * @property {number} currentTime
+     *
      * 물리엔진에서는 가속도를 적분하여 속도를 나타내고,
      * 속도를 적분하여 이동거리를 나타내기 때문에
      * 정확한 연산을 위해서는 수식에서 사용할 ∇t가 일정해야한다.
      * 따라서 fixedDeltaTime은 이론적으로 1 프레임을 렌더링할 때
      * 걸리는 시간을 ∇t로 정한다.
      */
+    this.fixedDeltaTime = 0;
     this.setFixedDeltaTime();
   }
 
-  /*
+  /**
    * 현재 프레임과 이전 프레임간의 시간차를 구해 deltaTime에 저장하고,
    * 그 값을 accumulatedTime에 누적한다.
    *
@@ -98,15 +113,29 @@ export default class Timer {
     this.previousTime = this.currentTime;
   }
 
+  /**
+   * 브라우저가 열린 후 또는 새로고침된 후 지난 시간을 반환한다.
+   *
+   * @returns {number} 초 단위의 지난 시간
+   */
   getCurrentTime() {
     return performance.now() / 1000;
   }
 
+  /**
+   * 1초에 렌더링할 프레임의 개수를 설정한다.
+   *
+   * @param {number} fps - 프레임의 개수
+   */
   setFps(fps) {
     this.fps = typeCheckAndClamp(fps, "number", 60, 24, Number.MAX_VALUE);
     this.setFixedDeltaTime();
   }
 
+  /**
+   * 물리엔진을 업데이트하기위한 fixedDeltaTime을 설정한다.
+   * 이 때 Timer의 fps속성을 이용하여 계산한다.
+   */
   setFixedDeltaTime() {
     this.fixedDeltaTime = 1 / this.fps;
   }
